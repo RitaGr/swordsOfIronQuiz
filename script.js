15d256e77d3aff6c12f92953b54a4f4900934b3b
vika_s <vikas@pango.co.il>
2023-11-01 14:58:31 +0200

    Create initial template of the quiz

---
+++ script.js
@@ -0,0 +1,7 @@
+const questions = [{
+    "question": '“From the River to the Sea Palestine Will Be Free” is a catch-all phrase symbolizing Palestinian control over the entire territory of Israel\'s borders, from the Jordan River to the Dead Sea',
+    "response": "You look so dumb right now...",
+    "answer": false
+}];
+
+document.getElementById("questionText").innerHTML = questions[0].question;
